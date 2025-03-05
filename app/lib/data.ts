3059ac4d1f43c32b38a063ce834d9271ab7703ad
adelante <pjhkaka@gmail.com>
2025-03-05 18:39:19 +0900

    qq

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -81,7 +81,12 @@ export async function fetchCardData() {
     };
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error(`Failed to fetch card data. ${error}`);
+    // throw new Error(`Failed to fetch card data. ${error}`);
+    return {
+      numberOfCustomers: '0',
+      numberOfInvoices: '0',
+      totalPaidInvoices: '0',
+      totalPendingInvoices: '0',}
   }
 }
 
